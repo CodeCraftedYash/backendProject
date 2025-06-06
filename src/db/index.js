d01f7fcb6ad421ca8edd87cfa872295b021334f8
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-06-06 19:04:05 +0530

    connect to database

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const clientOptions = {
+  serverApi: {
+    version: '1',
+    strict: true,
+    deprecationErrors: true
+  }
+};
+
+export default async function connectDB() {
+  try {
+    const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}/?retryWrites=true&w=majority&appName=db`, clientOptions);
+    console.log(`✅ MongoDB connected successfully to: ${connection.connection.name}`);
+  } catch (error) {
+    console.error('❌ Error connecting to MongoDB:', error.message);
+    process.exit(1);
+  }
+}
