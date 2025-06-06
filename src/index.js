5a1135ef5ff2dde290f13d98d97fa9e50991da3f
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-06-07 04:04:29 +0530

    utils code disscussion course part

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 dotenv.config();
 
-connectDB();
+connectDB()
+.then(() => {
+ app.listen(process.env.PORT || 8000),() => {
+    console.log("Serverf is running at port :",process.env.PORT || 8000);
+ }
+})
+.catch((err) => {console.error('❌ Error during database connection:', err.message); process.exit(1); });
